f8100500a7f7aff68edc4fe44a80c35a28807965
qqdebianstable <qq@qq.qq>
2021-06-14 22:04:17 +0200

    Added sanity check after building musl

--- build_musl.sh
+++ build_musl.sh
@@ -10,6 +10,6 @@ if [[ ! -f musl-1.2.2.tar.gz ]]; then
 	tar xf "musl-1.2.2.tar.gz"
 fi
 cd "musl-1.2.2"
-CROSS_COMPILE="llvm-" ./configure --prefix="$LFS_PREFIX" --libdir="$LFS_SYSROOT/lib" --host="$LFS_TGT"
+CROSS_COMPILE="llvm-" ./configure --prefix="$LFS_PREFIX" --syslibdir="$LFS_SYSROOT/lib" --host="$LFS_TGT"
 make -j"$PARALLEL_JOBS"
 make install
---
+++ musl_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. vars.sh
+
+# Temporarly copy shared libraries: libunwind.so, libc++.so libc++abi.so
+# into sysroot/usr/lib. Right now they carry dependency to hosts's glibc
+# but will be built against the freshly bootstrapped musl
+# My clang lives inside /opt/clang_second, so:
+# cp /opt/clang_second/lib/lib*so* "$LFS_SYSROOT"/usr/lib/
+set -e
+
+cd "${LFS_SOURCES}"
+echo -e '#include<stdio.h>\nint main(){puts("foobar");return 0;}' > foo.c
+clang foo.c --sysroot="$LFS_SYSROOT"
+"${LFS_SYSROOT}/lib/ld-musl-x86_64.so.1" ./a.out
