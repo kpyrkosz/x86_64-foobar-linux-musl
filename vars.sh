f342cfa6d4f04fbeddac1fafa8897bff39f16553
qqdebianstable <qq@qq.qq>
2021-06-14 22:30:40 +0200

    Added linux headers

--- vars.sh
+++ vars.sh
@@ -14,3 +14,5 @@ export LFS_TGT="${LFS_ARCH}-${LFS_VENDOR}-linux-musl"
 export LFS_PREFIX="${LFS_SYSROOT}/usr"
 export LFS_SOURCES="${LFS_SYSROOT}/sources"
 export PARALLEL_JOBS=5
+#I keep my llvm source tree there, I'm going to omit cloning the git repository in the script
+export LLVM_SOURCE_TREE=/usr/local/src/llvm_project/
