246f3e8e0a7e2e93f40805530de62f9294e7aed1
Krzysztof <krzpyrkosz@gmail.com>
2021-06-15 00:31:47 +0200

    Building tar

---
+++ build_tar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+. vars.sh
+. helper_fetch_and_build.sh
+
+fetch_and_unpack "http://ftp.gnu.org/gnu/tar/tar-1.34.tar.xz"
+CFLAGS="--sysroot=$LFS_SYSROOT" ./configure --prefix="$LFS_PREFIX" --host="$LFS_TGT"
+make -j"$PARALLEL_JOBS"
+make install
